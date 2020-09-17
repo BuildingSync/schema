c57fa53120e8df29ec8b0c169aa32c5ad1c64d3e
Ted Summer <ted.summer2@gmail.com>
2020-09-17 09:54:25 -0600

    fix(spec): download geojson xsd before validating files

--- spec/validation_spec.rb
+++ spec/validation_spec.rb
@@ -1,10 +1,33 @@
 require 'nokogiri'
 require 'pp'
+require 'open-uri'
 
 
 RSpec.describe 'Validate Examples' do
   before :all do
-    @xsd = Nokogiri::XML::Schema(File.read('BuildingSync.xsd'))
+    # Nokogiri doesn't seem to support XSDs which import other schemas with URLs
+    # for the schemaLocation. To allow testing, we download the imported schema
+    # and point schemaLocation to it instead
+    schema_doc = Nokogiri::XML(File.read('BuildingSync.xsd'))
+
+    GEOJSON_XSD_PATH = 'geojson.xsd'
+    GEOJSON_IMPORT_XPATH = 'xs:schema/xs:import[@namespace = "http://www.gbxml.org/schema"]'
+    if !File.file?(GEOJSON_XSD_PATH) then
+      imported_schema_locations = schema_doc.xpath(GEOJSON_IMPORT_XPATH).collect { |nokogiri_xml_node|
+        nokogiri_xml_node.attribute("schemaLocation").value
+      }
+      expect(imported_schema_locations.length).to eq 1
+
+      open(GEOJSON_XSD_PATH, 'wb') do |file|
+        file << open(imported_schema_locations[0]).read
+      end
+    end
+
+    schema_doc.xpath(GEOJSON_IMPORT_XPATH).collect { |nokogiri_xml_node|
+      nokogiri_xml_node.attribute("schemaLocation").value = GEOJSON_XSD_PATH
+    }
+
+    @xsd = Nokogiri::XML::Schema.from_document(schema_doc)
   end
 
   it 'should validate xmls' do
