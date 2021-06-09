cee17acdb6dc9202e3b0add93ef0df75c95b6f4c
Xiong <jie.xiong@nrel.gov>
2021-06-09 12:56:07 -0600

    Initial implementation
    Create proposal
    Implement in schema
    Transform examples

---
+++ translation/xsltproc.py
@@ -0,0 +1,27 @@
+import os
+import glob
+import lxml.etree as ET
+
+### Inputs
+# Path to the xsl file
+xsl_path = "v2_to_v3.xsl"
+# Path to the working folder
+example_path = '..\examples'
+
+### Prepare processor
+xslt = ET.parse(xsl_path)
+transform = ET.XSLT(xslt)
+
+### Transform files in the working folder and output in current folder
+for filename in glob.glob(os.path.join(example_path, '*.xml')):
+    
+    print('Working on ' + filename)
+    
+    xml_origin = ET.parse(filename)
+    xml_new = transform(xml_origin)
+
+    # Path to the output xml file
+    output_path = os.path.basename(filename)
+
+    with open(output_path, "wb") as f:
+        f.write(ET.tostring(xml_new, pretty_print=True))
