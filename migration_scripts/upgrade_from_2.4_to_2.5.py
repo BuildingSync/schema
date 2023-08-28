7361a79b71a3882336c6898c445ceae79ab04db3
Hannah Eslinger <heslinge@nrel.gov>
2023-08-28 16:41:52 -0600

    Address comments

--- migration_scripts/upgrade_from_2.4_to_2.5.py
+++ migration_scripts/upgrade_from_2.4_to_2.5.py
@@ -2,28 +2,57 @@
 Simply makes UsefulLife an int.
 """
 import argparse
+import logging
 from lxml import etree as ET
+
+
+logging.basicConfig(level=logging.INFO)
+
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("file_path", type=str, help="path to file to update")
+    parser.add_argument("from_file", type=str, help="path to file to update")
+    parser.add_argument(
+        "--to_file",
+        type=str,
+        help="path to write update to",
+        required=False,
+        default=None,
+    )
     return parser.parse_args()
 
+
+def update_version(tree):
+    tree.getroot().set("version", "2.5.0")
+
+
 def update_usefulLife(tree):
-    for usefulLife in tree.findall('.//*{http://buildingsync.net/schemas/bedes-auc/2019}UsefulLife'):
-        usefulLife.text = usefulLife.text.split(".", 1)[0]
+    for usefulLife in tree.findall(
+        ".//*{http://buildingsync.net/schemas/bedes-auc/2019}UsefulLife"
+    ):
+        new_value = str(round(float(usefulLife.text)))
+        if new_value != usefulLife.text:
+            logging.info(
+                f"changing {tree.getelementpath(usefulLife)} value from {usefulLife.text} to {new_value}"
+            )
+            usefulLife.text = new_value
+
 
 def main():
-    file_path = get_args().file_path
+    from_file, to_file = get_args().from_file, get_args().to_file
 
     try:
-        tree = ET.parse(file_path)
+        tree = ET.parse(from_file)
     except (FileNotFoundError, ET.ParseError) as e:
         exit(f"File could not be read \n{e} \naborting...")
 
+    update_version(tree)
     update_usefulLife(tree)
-    
-    tree.write(file_path)
+
+    if to_file is None:
+        to_file = from_file
+
+    tree.write(to_file)
 
 
 if __name__ == "__main__":
